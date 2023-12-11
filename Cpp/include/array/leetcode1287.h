3d8ee11d22535d9ce5b3328df2531f9213f0b086
robertwencn <wenquan.hk@gmail.com>
2023-12-11 22:20:43 +0800

    add leetcode problem 1287 and test cases

---
+++ Cpp/include/array/leetcode1287.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+
+class Solution1287 {
+public:
+  int findSpecialInteger(std::vector<int>& arr) {
+    size_t size = arr.size() / 4 + 1;
+    int count = 0;
+    int now = arr[0];
+    for (auto e : arr) {
+      if (e == now) {
+        count++;
+        if (count >= size) {
+          return e;
+        }
+      } else {
+        now = e;
+        count = 1;
+      }
+    }
+
+    return now;
+  }
+};
