b59cf8f2ab33068744a23be07f4a12dc7e382a58
wenquanhk <wenquan.hk@gmail.com>
2023-12-12 01:04:07 +0800

    1.rename cpp project to cpp_programing\n 2.add rust_programing and leetcode 1464 and its testcases

---
+++ cpp_programing/include/top-interview-150/leetcode58.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+class Solution58 {
+public:
+  int lengthOfLastWord(std::string s) {
+    int pos = s.find_last_not_of(' ');
+    int ret = 0;
+    while (pos >= 0 && s[pos] != ' ') {
+      ret++;
+      pos--;
+    }
+
+    return ret;
+  }
+};
---
+++ rust_programing/src/main.rs
@@ -0,0 +1,6 @@
+use crate::array::leetcode1464;
+mod array;
+
+fn main() {
+    println!("Hello, world!");
+}
