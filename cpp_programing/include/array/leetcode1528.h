7a326f40b0fd81295c4535fdbd8f0eca5e4ee048
robertwencn <wenquan.hk@gmail.com>
2023-12-13 21:41:03 +0800

    add leetcode 1528 solution and testcases

---
+++ cpp_programing/include/array/leetcode1528.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include <numeric>
+
+class Solution1528 {
+public:
+  int numSpecial(std::vector<std::vector<int>>& mat) {
+    int sum = 0;
+    int pos = 0;
+    int ret = 0;
+    int rowSize = mat.size();
+    int colSize = mat[0].size();
+    for (int i = 0; i < colSize; ++i) {
+      for (int j = 0; j < rowSize; ++j) {
+        if (mat[j][i] == 1) {
+          sum++;
+          pos = j;
+        }
+      }
+      if ((sum == 1) && std::accumulate(mat[pos].begin(), mat[pos].end(), 0) == 1) {
+        ret++;
+      }
+      sum = 0;
+    }
+
+    return ret;
+  }
+};
