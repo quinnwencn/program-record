977df9c4d67e4517985db08b3b08099b0ba248c0
robertwencn <wenquan.hk@gmail.com>
2023-12-14 22:02:33 +0800

    add solution of leetcode 26 and testcases

---
+++ cpp_programing/include/array/leetcode26.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+
+class Solution26 {
+public:
+  int removeDuplicates(std::vector<int>& nums) {
+    if (nums.size() == 0) {
+      return 0;
+    }
+
+    std::unique(nums.begin(), nums.end());
+    int i = 0;
+    for (; i < nums.size() - 1; i++) {
+      if (nums[i] >= nums[i + 1]) {
+        break;
+      }
+    }
+
+    return i + 1;
+  }
+};
---
+++ cpp_programing/test/src/test_leetcode26.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Robert on 2023/12/14.
+//
+#include <gtest/gtest.h>
+
+#include <array/leetcode26.h>
+
+TEST(Leetocde26Test, case1) {
+    std::vector<int> input {1, 1, 2};
+    Solution26 s;
+    EXPECT_EQ(2, s.removeDuplicates(input));
+}
+
+TEST(Leetcode26Test, case2) {
+    std::vector<int> input {0,0,1,1,1,2,2,3,3,4};
+    Solution26 s;
+    EXPECT_EQ(5, s.removeDuplicates(input));
+}
