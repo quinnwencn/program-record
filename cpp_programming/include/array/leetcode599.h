480932d6ddb7ee3058f374ba57383fce1bfb8448
robertwenhk <robertwen.hk@gmail.com>
2024-02-07 12:18:39 +0800

    add another solution for leetcode problem 599
    Change-Id: Ie781f8114a08de144506fe2ffa0f5c06808e7d57

--- cpp_programming/include/array/leetcode599.h
+++ cpp_programming/include/array/leetcode599.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 class Solution599 {
 public:
@@ -25,6 +26,32 @@ public:
                     
                     ret.push_back(list1[i]); 
                 }
+            }
+        }
+        return ret;
+    }
+};
+
+class Solution599_2 {
+public:
+    std::vector<std::string> findRestaurant(std::vector<std::string>& list1, std::vector<std::string>& list2) {
+        std::unordered_map<std::string, uint16_t> indexMap;
+        for (uint16_t i = 0; i < list1.size(); ++i) {
+            indexMap[list1[i]] = i;
+        }
+
+        uint16_t minIndex = list1.size() + list2.size();
+        std::vector<std::string> ret;
+        for (uint16_t i = 0; i < list2.size(); ++i) {
+            if (indexMap.find(list2[i]) != indexMap.end()) {
+                auto j = indexMap[list2[i]];
+                if (i + j > minIndex) {
+                    continue;
+                } else if (i + j < minIndex) {
+                    ret.clear();
+                    minIndex = j + i;
+                }
+                ret.push_back(list2[i]);
             }
         }
         return ret;
--- cpp_programming/test/src/array/test_leetcode599.cpp
+++ cpp_programming/test/src/array/test_leetcode599.cpp
@@ -12,26 +12,58 @@ TEST(Leetcode599Test, case1) {
     vector<string> list1 {"Shogun","Tapioca Express","Burger King","KFC"};
     vector<string> list2 {"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
     Solution599 s;
+    Solution599_2 s2;
     auto ret = s.findRestaurant(list1, list2);
+    auto ret2 = s2.findRestaurant(list1, list2);
     EXPECT_EQ(ret.size(), 1);
     EXPECT_EQ(ret[0], "Shogun");
+    EXPECT_EQ(ret2.size(), 1);
+    EXPECT_EQ(ret2[0], "Shogun");
 }
 
 TEST(Leetcode599Test, case2) {
     vector<string> list1 {"Shogun","Tapioca Express","Burger King","KFC"};
     vector<string> list2 {"KFC","Shogun","Burger King"};
     Solution599 s;
+    Solution599_2 s2;
+    auto ret2 = s2.findRestaurant(list1, list2);
     auto ret = s.findRestaurant(list1, list2);
     EXPECT_EQ(ret.size(), 1);
     EXPECT_EQ(ret[0], "Shogun");
+    EXPECT_EQ(ret2.size(), 1);
+    EXPECT_EQ(ret2[0], "Shogun");
 }
 
 TEST(Leetcode599Test, case3) {
     vector<string> list1 {"happy","sad","good"};
     vector<string> list2 {"sad","happy","good"};
     Solution599 s;
+    Solution599_2 s2;
     auto ret = s.findRestaurant(list1, list2);
+    auto ret2 = s2.findRestaurant(list1, list2);
+    EXPECT_EQ(ret2.size(), 2);
+    EXPECT_TRUE(std::find(ret2.begin(), ret.end(), "sad") != ret2.end());
+    EXPECT_TRUE(std::find(ret2.begin(), ret.end(), "happy") != ret2.end());
     EXPECT_EQ(ret.size(), 2);
     EXPECT_TRUE(std::find(ret.begin(), ret.end(), "sad") != ret.end());
     EXPECT_TRUE(std::find(ret.begin(), ret.end(), "happy") != ret.end());
+}
+
+TEST(Leetcode599Test, case4) {
+    vector<string> list1 {"Shogun","Tapioca Express","Burger King","KFC"};
+    vector<string> list2 {"KNN","KFC","Burger King","Tapioca Express","Shogun"};
+    Solution599 s;
+    Solution599_2 s2;
+    auto ret = s.findRestaurant(list1, list2);
+    auto ret2 = s2.findRestaurant(list1, list2);
+    EXPECT_EQ(ret2.size(), 4);
+    EXPECT_TRUE(std::find(ret2.begin(), ret.end(), "Shogun") != ret2.end());
+    EXPECT_TRUE(std::find(ret2.begin(), ret.end(), "Tapioca Express") != ret2.end());
+    EXPECT_TRUE(std::find(ret2.begin(), ret.end(), "Burger King") != ret2.end());
+    EXPECT_TRUE(std::find(ret2.begin(), ret.end(), "KFC") != ret2.end());
+    EXPECT_EQ(ret.size(), 4);
+    EXPECT_TRUE(std::find(ret.begin(), ret.end(), "Shogun") != ret.end());
+    EXPECT_TRUE(std::find(ret.begin(), ret.end(), "Tapioca Express") != ret.end());
+    EXPECT_TRUE(std::find(ret.begin(), ret.end(), "Burger King") != ret.end());
+    EXPECT_TRUE(std::find(ret.begin(), ret.end(), "KFC") != ret.end());
 }
