8b547d8b5a9dac3fce7476a5925a7afab34a91b7
Quinn <quinnwen.cn@gmail.com>
2024-08-05 20:38:48 +0800

    add solution for leetcode 189
    Change-Id: I764044b94bee6f466513df377206e98bf8085819

--- cpp_programming/include/top-interview-150/leetcode189.h
+++ cpp_programming/include/top-interview-150/leetcode189.h
@@ -5,4 +5,28 @@
 #ifndef CPPPROGRAMRECORD_LEETCODE189_H
 #define CPPPROGRAMRECORD_LEETCODE189_H
 
+#include <vector>
+#include <algorithm>
+
+class Solution189 {
+public:
+    void rotate(std::vector<int>& nums, int k) {
+        k %= nums.size();
+
+        std::vector<int> res;
+        res.reserve(nums.size());
+        res.insert(res.begin(), nums.begin() + nums.size() - k, nums.end());
+        res.insert(res.end(), nums.begin(), nums.begin() + nums.size() - k);
+
+        std::swap(res, nums);
+    }
+
+    void rotate1(std::vector<int>& nums, int k) {
+        k %= nums.size();
+        std::reverse(nums.begin(), nums.end());
+        std::reverse(nums.begin(), nums.begin() + k);
+        std::reverse(nums.begin() + k, nums.end());
+    }
+};
+
 #endif //CPPPROGRAMRECORD_LEETCODE189_H
--- cpp_programming/test/src/top-interview-150/test_leetcode189.cpp
+++ cpp_programming/test/src/top-interview-150/test_leetcode189.cpp
@@ -1,3 +1,36 @@
 //
 // Created by Quinn on 2024/8/5.
 //
+#include "top-interview-150/leetcode189.h"
+
+#include <gtest/gtest.h>
+
+TEST(Leetcode189Test, Testcase1) {
+    std::vector<int> nums { 1, 2, 3};
+    int k = 4;
+
+    std::vector<int> rotated { 3, 1, 2};
+    Solution189 so;
+    so.rotate(nums, k);
+    EXPECT_EQ(nums, rotated);
+}
+
+TEST(Leetcode189Test, Testcase2) {
+    std::vector<int> nums { 1};
+    int k = 3;
+
+    std::vector<int> rotated { 1};
+    Solution189 so;
+    so.rotate(nums, k);
+    EXPECT_EQ(nums, rotated);
+}
+
+TEST(Leetcode189Test, Testcase3) {
+    std::vector<int> nums {1, 2, 3, 4, 5, 6, 7, 8};
+    int k = 3;
+
+    std::vector<int> rotated {6, 7, 8, 1, 2, 3, 4, 5};
+    Solution189 so;
+    so.rotate1(nums, k);
+    EXPECT_EQ(nums, rotated);
+}
